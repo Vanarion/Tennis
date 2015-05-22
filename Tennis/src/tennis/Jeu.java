656d7d4eb3a11f4ef80d3b6e19ff87d0dd8267a5
Vanarion <damien.perniquoski@epsi.fr>
2015-05-22 12:10:21 +0200

    Update Jeu.java

--- Tennis/src/tennis/Jeu.java
+++ Tennis/src/tennis/Jeu.java
@@ -2,19 +2,20 @@ package tennis;
 
 public class Jeu
 {
-	// Déclarations des variables
+	// déclarations des variables
 	private int ScoreJoueur1 = 0;
 	private int ScoreJoueur2 = 0;
 	private String nomJoueur2;
 	private String nomJoueur1;
 	
-	// Constructeur
+	// constructeur
 	public Jeu(String playerOneName, String playerTwoName)
 	{
 		this.nomJoueur1 = playerOneName;
 		this.nomJoueur2 = playerTwoName;
 	}
 
+	// renvoie le score
 	public String getScore()
 	{
 		if (hasWinner())
@@ -43,17 +44,6 @@ public class Jeu
 		return ScoreJoueur1 >= 3 && ScoreJoueur2 == ScoreJoueur1;
 	}
 	
-	private String playerWithHighestScore()
-	{
-		if (ScoreJoueur1 > ScoreJoueur2) {
-			return nomJoueur1;
-		} 
-		else
-		{
-			return nomJoueur2;
-		}
-	}
-	
 	private boolean hasWinner() 
 	{
 		if(ScoreJoueur2 >= 4 && ScoreJoueur2 >= ScoreJoueur1 + 2 )
@@ -72,11 +62,24 @@ public class Jeu
 		return false;
 	}
 
+	private String playerWithHighestScore()
+	{
+		if (ScoreJoueur1 > ScoreJoueur2) {
+			return nomJoueur1;
+		} 
+		else
+		{
+			return nomJoueur2;
+		}
+	}
+
+	// incrémente la variable ScoreJoueur1
 	public void playerOneScores()
 	{
 		ScoreJoueur1++;
 	}
 
+	// incrémente la variable ScoreJoueur2
 	public void playerTwoScores()
 	{
 		ScoreJoueur2++;
